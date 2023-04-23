e59b6f3b4abd84fbb92c439de23d0439d962c526
pravar18 <pravar.sharma@gmail.com>
2023-04-23 11:48:56 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import "./index.css";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
