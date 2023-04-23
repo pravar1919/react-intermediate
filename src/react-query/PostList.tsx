0dace1cb7a9b739c2c54fe8cac69f9d96d2344ae
pravar18 <pravar.sharma@gmail.com>
2023-04-23 12:28:36 +0530

    filter posts with userid

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,34 +1,33 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+import { useState } from "react";
+import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
+  const [userId, setUserId] = useState<number>();
+  const { data: posts, isLoading, error } = usePosts(userId);
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
-
-  if (error) return <p>{error}</p>;
+  if (isLoading) return <p>Loading....</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
-    <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select
+        className="form-select mb-3"
+        onChange={(event) => setUserId(parseInt(event.target.value))}
+        value={userId}
+      >
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
