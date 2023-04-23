e59e40bd883f6fa95cef52d2336f7b8c499d1041
pravar18 <pravar.sharma@gmail.com>
2023-04-23 12:48:39 +0530

    added pagination in the posts

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -8,19 +8,27 @@ interface Post {
   userId: number;
 }
 
-const usePosts = (userId: number | undefined) => {
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) => {
   const fetchPosts = () =>
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
-        params: { userId },
+        params: {
+          _start: (query.page - 1) * query.pageSize,
+          _limit: query.pageSize,
+        },
       })
       .then((res) => res.data);
 
   return useQuery<Post[], Error>({
-    // users/1/posts
-    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryKey: ["posts", query],
     queryFn: fetchPosts,
     staleTime: 1 * 60 * 1000, //1m
+    keepPreviousData: true, // seamless loading
   });
 };
 
