79d4345dd16539982e2bb2d8448835e2d47ae694
pravar18 <pravar.sharma@gmail.com>
2023-04-23 12:01:46 +0530

    added react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "./index.css";
 
@@ -11,6 +12,21 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
+
+// Query client configurations:
+// {
+//   defaultOptions: {
+//     queries: {
+//       retry: 3,
+//       cacheTime: 300_00, //5m
+//       staleTime: 5 * 1000, //5 seconds
+//       refetchOnWindowFocus: false,
+//       refetchOnReconnect: false,
+//       refetchOnMount: false,
+//     },
+//   },
+// }
